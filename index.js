a74b9129a266f2ad225d3728429a25da0c4d150c
snehamoybag <snehamoybag2@gmail.com>
2024-10-04 19:59:11 +0530

    feat: allow sendPage() to recieve a status code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("node:http");
 const fs = require("node:fs");
 
-const sendPage = (res, pagePath = "/") => {
+const sendPage = (res, pagePath = "/", statusCode = 200) => {
   fs.readFile(pagePath, (error, data) => {
     if (error) {
       console.log(error);
@@ -9,7 +9,7 @@ const sendPage = (res, pagePath = "/") => {
       return;
     }
 
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(statusCode, { "Content-Type": "text/html" });
     res.write(data);
     res.end();
     return;
@@ -21,19 +21,19 @@ const server = http.createServer();
 server.on("request", (req, res) => {
   switch (req.url) {
     case "/":
-      sendPage(res, "./index.html");
+      sendPage(res, "./index.html", 200);
       break;
 
     case "/about":
-      sendPage(res, "./about.html");
+      sendPage(res, "./about.html", 200);
       break;
 
     case "/contact-me":
-      sendPage(res, "./contact-me.html");
+      sendPage(res, "./contact-me.html", 200);
       break;
 
     default:
-      sendPage(res, "./404.html");
+      sendPage(res, "./404.html", 404);
       break;
   }
 });
